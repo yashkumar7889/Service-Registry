13005665aa05b83795e0bb340e91203b31e401e5
Yash Kumar Singh Patwa <yashkumarsinghpatwa7889@gmail.com>
2024-01-03 21:10:07 +0530

    Service Registry Repository

---
+++ src/test/java/com/microservice/question/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.question.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
